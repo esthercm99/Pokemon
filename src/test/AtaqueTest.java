b67520ac5a4dc05cbf12e68e1b4d3356b517eef1
Esther <esthercmprogramacion@gmail.com>
2019-05-26 07:42:59 +0200

    Cajas negras

--- src/test/AtaqueTest.java
+++ src/test/AtaqueTest.java
@@ -1,14 +1,27 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
+import java.util.Map;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import modelo.pokemon.Ataque;
+import modelo.pokemon.Tipo;
+
+
 class AtaqueTest {
+	@SuppressWarnings("unchecked")
+	private Map<Tipo, Double> mapa = mock(Map.class);
+	private Tipo tipo = mock(Tipo.class);
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void constructorTest() {
+		String [] s = {"", "", ""};
+		
+		Assertions.assertThrows(NullPointerException.class, () -> new Ataque(null, null, null));
+		Assertions.assertThrows(IllegalArgumentException.class, () -> new Ataque(s, tipo, mapa));
 	}
 
 }
